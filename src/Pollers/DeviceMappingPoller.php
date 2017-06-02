e5d915a118ff2181e3c5a061fa089fda5dec1a17
Sonar Software <simon@sonar.software>
2017-06-02 13:00:20 -0500

    Bring in a MAC formatting function

--- src/Pollers/DeviceMappingPoller.php
+++ src/Pollers/DeviceMappingPoller.php
@@ -5,10 +5,29 @@ namespace SonarSoftware\Poller\Pollers;
 
 class DeviceMappingPoller
 {
+    /**
+     * Poll a list of devices to determine their connections
+     * @param array $work
+     * @return array
+     */
     public function poll(array $work):array
     {
         $results = [];
-
         return $results;
+    }
+
+    /**
+     * Format a MAC in a variety of formats into a standardized, colon separated, upper case MAC
+     * @param $mac
+     * @return string
+     */
+    private function formatMac($mac)
+    {
+        $cleanMac = str_replace(" ","",$mac);
+        $cleanMac = str_replace("-","",$cleanMac);
+        $cleanMac = str_replace(":","",$cleanMac);
+        $cleanMac = strtoupper($cleanMac);
+        $macSplit = str_split($cleanMac,2);
+        return implode(":",$macSplit);
     }
 }
