8c2fd59b987a13857691d317820c21578412a772
Sonar Software <simon@sonar.software>
2017-06-05 17:56:31 -0500

    Add a Cambium ePMP AP mapper

---
+++ src/DeviceMappers/CambiumEpmpAccessPointMapper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SonarSoftware\Poller\DeviceMappers;
+
+use SonarSoftware\Poller\Formatters\Formatter;
+use SonarSoftware\Poller\Models\Device;
+
+class CambiumEpmpAccessPointMapper extends BaseDeviceMapper implements DeviceMapperInterface
+{
+
+    public function mapDevice(): Device
+    {
+        $this->setSystemMetadataOnDevice();
+        $arrayOfDeviceInterfacesIndexedByInterfaceIndex = $this->getInterfacesWithStandardMibData(true, false);
+        $arrayOfDeviceInterfacesIndexedByInterfaceIndex = $this->getConnectedRadios($arrayOfDeviceInterfacesIndexedByInterfaceIndex);
+        $this->device->setInterfaces($arrayOfDeviceInterfacesIndexedByInterfaceIndex);
+
+        return $this->device;
+    }
+
+    /**
+     * This gets attached to the WLAN interface, which is always the second interface.
+     * @param array $arrayOfDeviceInterfacesIndexedByInterfaceIndex
+     * @return array|mixed
+     */
+    private function getConnectedRadios(array $arrayOfDeviceInterfacesIndexedByInterfaceIndex):array
+    {
+        $result = $this->snmp->walk("1.3.6.1.4.1.17713.21.1.2.11.1.1");
+        foreach ($result as $key => $datum)
+        {
+            $mac = Formatter::formatMac($this->cleanSnmpResult($datum));
+            if ($this->validateMac($mac))
+            {
+                array_push($arrayOfDeviceInterfacesIndexedByInterfaceIndex[count($arrayOfDeviceInterfacesIndexedByInterfaceIndex)-1]['connected_l2'],$mac);
+            }
+        }
+        return $arrayOfDeviceInterfacesIndexedByInterfaceIndex;
+    }
+}
