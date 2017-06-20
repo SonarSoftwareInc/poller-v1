4e217e716b180db7ce61bf37a89a3a36f2eeeb7f
Sonar Software <simon@sonar.software>
2017-06-20 11:38:41 -0500

    Get the air capacity on Ubiquiti AirFiber

--- src/DeviceMappers/Ubiquiti/UbiquitiAirFiber.php
+++ src/DeviceMappers/Ubiquiti/UbiquitiAirFiber.php
@@ -17,11 +17,41 @@ class UbiquitiAirFiber extends BaseDeviceMapper implements DeviceMapperInterface
     public function mapDevice(): Device
     {
         $this->setSystemMetadataOnDevice();
-        $arrayOfDeviceInterfacesIndexedByInterfaceIndex = $this->getInterfacesWithStandardMibData(false, false, true, false, true);
+        $arrayOfDeviceInterfacesIndexedByInterfaceIndex = $this->getInterfacesWithStandardMibData(false, false, true, false, false);
         $arrayOfDeviceInterfacesIndexedByInterfaceIndex = $this->getRemoteBackhaulMac($arrayOfDeviceInterfacesIndexedByInterfaceIndex);
+        $arrayOfDeviceInterfacesIndexedByInterfaceIndex = $this->getWirelessCapacity($arrayOfDeviceInterfacesIndexedByInterfaceIndex);
         $this->device->setInterfaces($arrayOfDeviceInterfacesIndexedByInterfaceIndex);
 
         return $this->device;
+    }
+
+    /**
+     * Get the capacity of the wireless interface
+     * @param array $arrayOfDeviceInterfacesIndexedByInterfaceIndex
+     * @return array
+     */
+    private function getWirelessCapacity(array $arrayOfDeviceInterfacesIndexedByInterfaceIndex):array
+    {
+        $keyToUse = 1;
+        foreach ($arrayOfDeviceInterfacesIndexedByInterfaceIndex as $key => $deviceInterface)
+        {
+            if (strpos($deviceInterface->getDescription(),"air0") !== false)
+            {
+                $keyToUse = $key;
+                break;
+            }
+        }
+
+        try {
+            $capacity = $this->snmp->get(".1.3.6.1.4.1.41112.1.3.2.1.5");
+            $arrayOfDeviceInterfacesIndexedByInterfaceIndex[$keyToUse]->setSpeedMbps($this->cleanSnmpResult($capacity)/1000**2);
+        }
+        catch (Exception $e)
+        {
+            //
+        }
+
+        return $arrayOfDeviceInterfacesIndexedByInterfaceIndex;
     }
 
     /**
